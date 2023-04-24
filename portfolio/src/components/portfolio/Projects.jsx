bea4d7ef7c0e8835619951e5179fe7eb431cc3bd
Glenn Andaleon <andaleon34@gmail.com>
2023-04-24 21:12:04 +0800

    Update

---
+++ portfolio/src/components/portfolio/Projects.jsx
@@ -0,0 +1,22 @@
+
+
+export const projectsData = [
+    {
+        id: 1,
+        title: "VA HNTR",
+        description: "Effortlessly simplify your life with our virtual assistant.",
+        link: "https://group-6-project.vercel.app/"
+    },
+    {
+        id: 2,
+        title: "Parking Management System",
+        description: "GSix Parking Management System has long-standing experience in the field of modern parking management and can help cities make their transportation system more efficient and improve overall quality of life for their citizens.",
+        link: "https://mini-project-2-five.vercel.app/"
+    },
+    {
+        id: 3,
+        title: "PMS APP",
+        description: "Parking Management System (PMS) is an integrated smart parking system that automates end-to-end parking processes.",
+        link: "http://group6-capstone.infinityfreeapp.com/login/login.php"
+    }
+]
